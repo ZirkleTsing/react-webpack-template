0ee4dc3828581ae09361e4f8017a6b707fe24c0f
chang-mac <835702580@qq.com>
2017-11-29 16:02:10 +0800

    html-webpack-plugin

---
+++ build/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+  entry: {
+    app: path.join(__dirname, '../client/index.js')
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.join(__dirname, '../dist')
+    // publicPath: '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: [path.join(__dirname, '../node_modules')]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '../index.html'),
+      title: 'my webpack demo'
+    })
+  ]
+}
+
+module.exports = config
