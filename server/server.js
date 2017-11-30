f5e5f6b1ca605646275016b66c675446ea64d762
chang-mac <835702580@qq.com>
2017-11-30 15:12:59 +0800

    SSR development environment

--- server/server.js
+++ server/server.js
@@ -2,18 +2,27 @@ const express = require('express')
 const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
-const serverEntry = require('../dist/server-entry').default
+const serverSsrRender = require('./serverSSR')
 
 const app = express()
 
-app.use( '/public', express.static(path.join(__dirname, '../dist')))
+const isDev = process.env.NODE_ENV === 'production'
 
-app.get('*', (req, res) => {
-  const renderString = ReactSSR.renderToString(serverEntry)
-  let template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf-8')
-  template = template.replace('<!-- ssr -->', renderString)
-  res.send(template)
-})
+
+if(isDev) {
+  app.use( '/public', express.static(path.join(__dirname, '../dist')))
+  
+  app.get('*', (req, res) => {
+    const serverEntry = require('../dist/server-entry').default
+    const renderString = ReactSSR.renderToString(serverEntry)
+    let template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf-8')
+    template = template.replace('<!-- ssr -->', renderString)
+    res.send(template)
+  })
+} else {
+  serverSsrRender(app)
+}
+
 
 app.listen(3333, ()=> {
   console.log('server listening on 3333')
---
+++ server/serverSSR.js
@@ -0,0 +1,67 @@
+const axios = require('axios')
+const serverConfig = require('../build/webpack.config.server')
+const MemoryFS = require("memory-fs")
+const path = require('path')
+const webpack = require('webpack')
+const ReactSSR = require('react-dom/server')
+const proxy = require('http-proxy-middleware')
+
+
+// 1.从client服务端内存中取出index.html
+// 2.利用webpack()编译出SSR
+// 3.转化成字符串
+// 4.拼接;send
+
+
+const getTemplate = () => new Promise((resolve, reject) => {
+  axios.get('http://localhost:8888/public/index.html')
+    .then((res) => {
+      resolve(res.data)
+    })
+})
+const mfs = new MemoryFS()
+const serverComplier = webpack(serverConfig)
+// hack写法
+const Module = module.constructor
+let ssrBundle = null
+
+serverComplier.outputFileSystem = mfs
+serverComplier.watch({}, (err, stats) => {
+  if(err) throw err
+  stats = stats.toJson()
+  stats.errors.forEach(err => console.error(err))
+  stats.warnings.forEach(warn => console.warn(warn))
+
+  // 获取路径，此时已经在内存中存放了文件
+  const serverEntryPath = path.join(
+    serverConfig.output.path,
+    serverConfig.output.filename
+  )
+  // console.log('路径:', serverEntryPath)
+  const serverBundle = mfs.readFileSync(serverEntryPath, 'utf-8')
+  // console.log("Bundle:", serverBundle)
+  const m = new Module()
+  m._compile(serverBundle, 'server-entry.js')
+  // 这里已经取到了服务端渲染的APP
+  ssrBundle = m.default
+  console.log(ssrBundle,'ssrBundle')
+})
+
+
+module.exports = (app) => {
+  app.use('/public', proxy({
+    target: 'http://localhost:8888/'
+  }))
+  app.get('*', (req, res) => {
+    getTemplate()
+      .then( template => {
+        // console.log('template: ', template)
+        const HtmlTemplate = template
+        // console.log(123, ssrBundle)
+        const renderString = ReactSSR.renderToString(ssrBundle)
+        // console.log(123, renderString)
+        // console.warn(HtmlTemplate.replace('<!-- ssr -->', renderString))
+        res.send(HtmlTemplate.replace('<!-- ssr -->', renderString))
+      })
+  })
+}
