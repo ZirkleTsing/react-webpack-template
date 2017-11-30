25a55b98d1de4745c4e65f03bd494bbb684c5e44
chang-mac <835702580@qq.com>
2017-11-30 15:13:31 +0800

    test

--- client/App.js
+++ client/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>this is app</div>
+      <div>this is app url42143aaaaas</div>
     )
   }
 }
