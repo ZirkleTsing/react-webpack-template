50603d7b815c6e50b04b2641deb7c9280ff288e7
chang-mac <835702580@qq.com>
2017-12-01 12:15:43 +0800

    test

--- client/App.js
+++ client/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
-let demo
 
 export default class App extends Component {
+  componentDidMount() {
+    // some
+  }
+
   render() {
     return (
       <div>this is app</div>
